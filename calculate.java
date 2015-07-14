0d7b1cfac33f86877ddd5aa4359f4f0861383f5d
unknown <I.A.Vasilyev@gmail.com>
2015-07-14 21:12:29 +0300

    add user interface calculator

--- calculate.java
+++ calculate.java
@@ -1,5 +1,8 @@
 public class calculate{
 	public static void main (String[] arg){
-		System.out.println(arg[0]);
+		System.out.println("Calculate sum");
+		int arg1 = Integer.valueOf(arg[0]);
+		int arg2 = Integer.valueOf(arg[1]);
+		System.out.println("Sum:" + (arg1+arg2));
 	}
 }
---
+++ userInteract.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+	Realease dialog with user for calculator
+*/
+public class userInteract{
+	public static void main (String[] arg){
+		Scanner reader = new Scanner(System.in);
+		try{
+			calculator calc = new calculator();
+			String exit;
+			do{
+				System.out.println("Enter args: ");
+				String arg1 = reader.next();
+				System.out.println("First arg is " + arg1);
+				
+				String arg2 = reader.next();
+				System.out.println("Second arg is " + arg2);
+				
+				calc.add(Integer.valueOf(arg1), Integer.valueOf(arg2));
+				System.out.println("Result: " + calc.getResult());
+				
+				System.out.println("Exit (yes/no): ");
+				exit = reader.next();
+				
+			} while (!exit.equals("yes"));
+		}
+		finally{
+			reader.close();
+		}
+	}
+}
